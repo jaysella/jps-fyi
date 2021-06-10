28daddb9621d41b17805af3c067b856f7666e8cc
Jay Sella <jay@jaysella.dev>
2021-06-10 16:48:12 -0400

    custom 404/500 error pages

---
+++ pages/404.js
@@ -0,0 +1,53 @@
+import Head from "next/head";
+import styled from "@emotion/styled";
+
+export default function Custom404() {
+  return (
+    <>
+      <Head>
+        <title>Error 404</title>
+      </Head>
+
+      <PageWrapper>
+        <Main>
+          <Inner>
+            <h2>😢</h2>
+            <h1>Error 404</h1>
+            <p>Page Not Found</p>
+          </Inner>
+        </Main>
+      </PageWrapper>
+    </>
+  );
+}
+
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  border: 2px solid var(--color-black);
+  border-radius: var(--base-border-radius);
+  margin: 2.5rem;
+  height: calc(100vh - (2.5rem * 2));
+  overflow: hidden;
+`;
+
+const Main = styled.main`
+  padding: 2.5rem;
+  overflow: auto;
+  text-align: center;
+`;
+
+const Inner = styled.div`
+  width: max-content;
+  margin: 0 auto;
+
+  h2 {
+    font-size: clamp(32px, 5vw, 64px);
+  }
+
+  h1 {
+    font-size: clamp(46px, 7.5vw, 92px);
+  }
+`;
