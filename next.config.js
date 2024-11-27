363a016aec8b8687d9b59e2160ed6b32dd8c5548
Jay Sella <35548468+jaysella@users.noreply.github.com>
2024-11-27 16:01:28 -0500

    Switch from ts to js

---
+++ next.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "media.jps.fyi",
+          },
+        ],
+        destination: "https://www.jps.fyi/wall",
+      },
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "api.jps.fyi",
+          },
+        ],
+        destination: "https://www.jps.fyi/wall",
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "media.jps.fyi",
+          },
+        ],
+        destination: "https://res.cloudinary.com/jaysella/:path*",
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "api.jps.fyi",
+          },
+        ],
+        destination: "/api/:path*",
+      },
+      {
+        source: "/m/:path",
+        destination: "https://res.cloudinary.com/jaysella/:path*",
+      },
+    ];
+  },
+};
