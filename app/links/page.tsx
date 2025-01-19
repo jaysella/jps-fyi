748c4fb2981e8ea596e568a9b6117d5e43302193
Jay Sella <35548468+jaysella@users.noreply.github.com>
2025-01-19 14:19:50 -0500

    Move auth to middleware for new app router pages

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -1,4 +1,3 @@
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { Suspense } from "react";
 import { LinksSection } from "./links.server";
 
@@ -6,23 +5,20 @@ export const metadata = {
   title: "Links",
 };
 
-export default withPageAuthRequired(
-  async function LinksPage() {
-    return (
-      <div className="container mx-auto py-10">
-        <div className="space-y-8">
-          <div>
-            <h1 className="text-2xl font-bold tracking-tight">Links</h1>
-            <p className="text-muted-foreground">Manage shortened URLs</p>
-          </div>
-          <div>
-            <Suspense fallback={"Loading..."}>
-              <LinksSection />
-            </Suspense>
-          </div>
+export default function LinksPage() {
+  return (
+    <div className="container mx-auto py-10">
+      <div className="space-y-8">
+        <div>
+          <h1 className="text-2xl font-bold tracking-tight">Links</h1>
+          <p className="text-muted-foreground">Manage shortened URLs</p>
+        </div>
+        <div>
+          <Suspense fallback={"Loading..."}>
+            <LinksSection />
+          </Suspense>
         </div>
       </div>
-    );
-  },
-  { returnTo: "/links" }
-);
+    </div>
+  );
+}
