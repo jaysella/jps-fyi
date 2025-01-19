e94bd80fd388b92d83268bc9db9085cbd8c45a60
Jay Sella <35548468+jaysella@users.noreply.github.com>
2025-01-19 14:53:27 -0500

    Enable eslint.ignoreDuringBuilds

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,11 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   async rewrites() {
     return [
       {
