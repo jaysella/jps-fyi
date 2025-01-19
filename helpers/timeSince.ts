380055f1d27eb8d23a34123b2ecaa9f8ec0da84f
Jay Sella <35548468+jaysella@users.noreply.github.com>
2025-01-19 14:28:54 -0500

    Don't show number of seconds since timestamp

--- helpers/timeSince.ts
+++ helpers/timeSince.ts
@@ -7,30 +7,30 @@ export function timeSince(date) {
 
   let interval = Math.floor(seconds / 31536000);
   if (interval > 1) {
-    return interval + " year" + numberEnding(interval);
+    return interval + " year" + numberEnding(interval) + " ago";
   }
 
   interval = Math.floor(seconds / 2592000);
   if (interval > 1) {
-    return interval + " month" + numberEnding(interval);
+    return interval + " month" + numberEnding(interval) + " ago";
   }
 
   interval = Math.floor(seconds / 86400);
   if (interval > 1) {
-    return interval + " day" + numberEnding(interval);
+    return interval + " day" + numberEnding(interval) + " ago";
   }
 
   interval = Math.floor(seconds / 3600);
   if (interval > 1) {
-    return interval + " hour" + numberEnding(interval);
+    return interval + " hour" + numberEnding(interval) + " ago";
   }
 
   interval = Math.floor(seconds / 60);
   if (interval > 1) {
-    return interval + " minute" + numberEnding(interval);
+    return interval + " minute" + numberEnding(interval) + " ago";
   }
 
-  return Math.floor(seconds) + " second" + numberEnding(Math.floor(seconds));
+  return "just now";
 }
 
 export function timeSinceFromTimestamp(timestamp) {
