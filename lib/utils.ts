6103f1a3a4c50c9b7d69cdb4d1921512209c7110
Jay Sella <35548468+jaysella@users.noreply.github.com>
2025-01-19 16:11:53 -0500

    Handle zero intervals and fix timestamp type

--- lib/utils.ts
+++ lib/utils.ts
@@ -13,33 +13,33 @@ export function timeSince(date: Date | number) {
   }
 
   let interval = Math.floor(seconds / 31536000);
-  if (interval > 1) {
+  if (interval > 0) {
     return interval + " year" + numberEnding(interval) + " ago";
   }
 
   interval = Math.floor(seconds / 2592000);
-  if (interval > 1) {
+  if (interval > 0) {
     return interval + " month" + numberEnding(interval) + " ago";
   }
 
   interval = Math.floor(seconds / 86400);
-  if (interval > 1) {
+  if (interval > 0) {
     return interval + " day" + numberEnding(interval) + " ago";
   }
 
   interval = Math.floor(seconds / 3600);
-  if (interval > 1) {
+  if (interval > 0) {
     return interval + " hour" + numberEnding(interval) + " ago";
   }
 
   interval = Math.floor(seconds / 60);
-  if (interval > 1) {
+  if (interval > 0) {
     return interval + " minute" + numberEnding(interval) + " ago";
   }
 
   return "just now";
 }
 
-export function timeSinceFromTimestamp(timestamp: number) {
+export function timeSinceFromTimestamp(timestamp: number | string) {
   return timeSince(new Date(timestamp).getTime());
 }
