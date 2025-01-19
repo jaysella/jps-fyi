c0a8b76f648e59f133c2f5c38a941011a1107bc7
Jay Sella <35548468+jaysella@users.noreply.github.com>
2025-01-19 16:01:56 -0500

    Streamline time since logic

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,45 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function timeSince(date: Date | number) {
+  const seconds = Math.floor((new Date().getTime() - (date instanceof Date ? date.getTime() : date)) / 1000);
+
+  function numberEnding(number: number) {
+    return number > 1 ? "s" : "";
+  }
+
+  let interval = Math.floor(seconds / 31536000);
+  if (interval > 1) {
+    return interval + " year" + numberEnding(interval) + " ago";
+  }
+
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " month" + numberEnding(interval) + " ago";
+  }
+
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " day" + numberEnding(interval) + " ago";
+  }
+
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hour" + numberEnding(interval) + " ago";
+  }
+
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minute" + numberEnding(interval) + " ago";
+  }
+
+  return "just now";
+}
+
+export function timeSinceFromTimestamp(timestamp: number) {
+  return timeSince(new Date(timestamp).getTime());
 }
